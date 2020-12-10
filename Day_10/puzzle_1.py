9764dd1494b46b0578c63513d04466fcc0806a42
JuanGonzalezCaminero <juangonzalez5@hotmail.es>
2020-12-10 16:03:10 +0100

    Day 10

--- Day_10/puzzle_1.py
+++ Day_10/puzzle_1.py
@@ -1,1 +1,18 @@
 
+file=open("input")
+data=[int(i) for i in file.readlines()]
+data.sort()
+
+current_joltage=0
+differences=[0,0,0]
+
+for adapter in data:
+	differences[adapter-current_joltage-1]+=1
+	current_joltage=adapter
+
+#Device adapter (3 jolt difference)
+differences[2]+=1
+current_joltage+=3
+
+print(differences)
+print(differences[0]*differences[2])
